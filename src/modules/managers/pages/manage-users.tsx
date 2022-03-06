14dd6ff5d487dfd530da1d5018eb7167c587db1c
Samuel Adebayo <sambayo28@gmail.com>
2022-03-06 22:28:50 +0100

    start data display modals

--- src/modules/managers/pages/manage-users.tsx
+++ src/modules/managers/pages/manage-users.tsx
@@ -1,10 +1,12 @@
 import { users } from '../../../data';
 import UsersTable from '../components/users-table';
+import Modal from '../components/modal';
 
 function ManageUsers() {
   return (
     <div className="manages-users">
       <UsersTable users={users} />
+      <Modal />
     </div>
   );
 }
